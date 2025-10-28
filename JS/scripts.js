a5407855e1b597a82e2e48d2fde52908d26a67a7
MarlaSinger2025 <Marla_Singer_2020@web.de>
2025-10-28 17:21:51 +0100

    addingComments
    for better understanding.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,6 @@
 function newItem(){
 
+// function for adding Items to the list via Add Button
 let li = $('<li></li>');
 
 let inputValue = $('#input').val();
@@ -11,12 +12,21 @@ if (inputValue === '') {
     $('#list').append(li);
 }
 
+// Crossing out Item when Double Click on it . With toggle another Double Click nullifies it
     li.on("dblclick" , function(){
     li.toggleClass("strike");
 });
 
+// Creating X Button for deleting List Item
 let crossOutButton = $('<crossOutButton></crossOutButton>');
 crossOutButton.append(document.createTextNode('X'));
 li.append(crossOutButton);
+
+// on = addEventListner in Vanilla JS
+crossOutButton.on("click", deleteListItem);
+
+function deleteListItem(){
+    li.addClass("delete");
+}
 
 }
