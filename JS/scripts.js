804f0643c1757d1fe68adeb5ce6ae3d0216ec349
MarlaSinger2025 <Marla_Singer_2020@web.de>
2025-10-29 14:48:37 +0100

    EnterKeyBugFixed
    New and working code at the bottom.
    
    Chatgpt helped figuring out that the default setting for pressing Enter key is reloading (and clearing) the page. By preventing this default action you can add an item via pressing the enter key!

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,42 +1,42 @@
-function newItem(){
+function newItem() {
 
-// function for adding Items to the list via Add Button
-let li = $('<li></li>');
+    // adding Items to the list via Add Button
+    let li = $('<li></li>'); 
 
-let inputValue = $('#input').val();
-li.append(inputValue);
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+    $('#input').val(''); //clearing input field
 
-if (inputValue === '') {
-    alert("You must write something!");
-} else {
-    $('#list').append(li);
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        $('#list').append(li);
+    }
+
+    // double click to cross out. toggle makes it switch between crossed out and not.
+    li.on("dblclick", function() {
+        li.toggleClass("strike");
+    });
+
+    // delete X button
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
+
+    crossOutButton.on("click", deleteListItem);
+    
+    function deleteListItem() {
+        li.addClass("delete");
+    }
+
+    // change order of items by dragging them
+    $('#list').sortable();
 }
 
-// Crossing out Item when Double Click on it . With toggle another Double Click nullifies it
-    li.on("dblclick" , function(){
-    li.toggleClass("strike");
-});
-
-// Creating X Button for deleting List Item
-let crossOutButton = $('<crossOutButton></crossOutButton>');
-crossOutButton.append(document.createTextNode('X'));
-li.append(crossOutButton);
-
-// on = addEventListner in Vanilla JS
-crossOutButton.on("click", deleteListItem);
-
-function deleteListItem(){
-    li.addClass("delete");
-}
-
-// Change order of items
-$('#list').sortable();
-
-}
-
-// How can I add an Item with pressing the Enter key? Tried around with code but its definitely not right
-$('#input').on("keypress", function(e){
-    if (e.which === 13) {
-        newItem();  
+$('#input').on("keypress", function(e) {
+    if (e.which === 13) {   // 13 means "Enter" key
+        // Help from chatgpt: Default setting for pressing enter key is reloading page
+        e.preventDefault(); // preventing Default = Now it works!
+        newItem();
     }
 });
